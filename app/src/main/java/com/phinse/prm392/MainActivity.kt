f4308326d1e9e48fa69e5812455f187f7d298556
PhiNSE <93396482+PhiNSE@users.noreply.github.com>
2024-05-19 21:31:08 +0700

    slot 4

--- app/src/main/java/com/phinse/prm392/MainActivity.kt
+++ app/src/main/java/com/phinse/prm392/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.phinse.prm392
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -27,6 +29,12 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,"Login successful", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this,"Login failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        findViewById<CardView>(R.id.ivInsta).setOnClickListener{
+            Intent(this, IstagramSignIn::class.java).apply {
+                startActivity(this)
             }
         }
     }
